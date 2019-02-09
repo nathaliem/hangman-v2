da77fceb2820b32182ea46bb956407421316fb31
Nathalie Maes <nathaliemaes92@gmail.com>
2019-02-09 22:45:36 +0100

    Back to normal

--- app/js/modules/answer/answer.js
+++ app/js/modules/answer/answer.js
@@ -21,9 +21,7 @@ const Answer = (() => {
     }
 
     const _obfuscateAnswer = () => {
-        answer = "No Man's Sky".toUpperCase();
         let wordArray = answer.split('');
-        console.log(wordArray);
         const obfuscated = wordArray.map(letter => {
             if (letter !== ' ' && letter !== ':' && letter !== '\'') {
                 return '_';
